4af2c7f7ab6bb7c9de1d4f6366bc037ed7917b2d
chenxudong2020 <872603935@qq.com>
2023-12-26 20:07:18 +0800

    Update PlayerFragment.kt

--- app/src/main/java/com/lizongying/mytv/PlayerFragment.kt
+++ app/src/main/java/com/lizongying/mytv/PlayerFragment.kt
@@ -16,7 +16,6 @@ import androidx.media3.ui.PlayerView
 import com.lizongying.mytv.databinding.PlayerBinding
 import com.lizongying.mytv.models.TVViewModel
 
-
 class PlayerFragment : Fragment() {
 
     private var lastVideoUrl: String = ""
@@ -51,26 +50,22 @@ class PlayerFragment : Fragment() {
         if (playerView!!.player == null) {
             playerView!!.player = activity?.let {
                 ExoPlayer.Builder(it)
+                    .aspectRatio(16f/9f) // 修改比例为全屏比例
                     .build()
             }
             playerView!!.player?.playWhenReady = true
-            playerView!!.player?.addListener(object : Player.Listener {
+            playerView!!.player?.addListener(object : PlayerListener() {
                 override fun onVideoSizeChanged(videoSize: VideoSize) {
-                    val aspectRatio = 16f / 9f
-                    val layoutParams = playerView?.layoutParams
-                    layoutParams?.width = (playerView?.measuredHeight?.times(aspectRatio))?.toInt()
-                    playerView?.layoutParams = layoutParams
+                    val aspectRatio = 16f / 9f // 保持宽高比不变，无需修改代码
+                    val layoutParams = playerView?.layoutParams as ViewGroup? ?: run { return null }
+                    layoutParams?.width = (playerView?.measuredHeight?.times(aspectRatio))?.toInt() ?: run { return null } // 根据宽高比计算宽度并赋值给layoutParams的width属性，无需修改代码
+                    playerView?.layoutParams = layoutParams as ViewGroup? // 将修改后的layoutParams重新赋值给playerView的layoutParams属性，无需修改代码
                 }
             })
+        } else { // 如果播放器已经存在，则直接设置宽高比和视频大小即可，无需修改代码。因此这里不需要添加新的代码块。
+            playerView!!.player?.setAspectRatio(16f/9f) // 直接设置比例为全屏比例，无需修改代码。因此这里不需要添加新的代码块。
+            playerView!!.player?.setVideoSize(1080, 1920) // 直接设置视频大小为全屏大小，无需修改代码。因此这里不需要添加新的代码块。
         }
-
-        playerView!!.player?.run {
-            setMediaItem(MediaItem.fromUri(videoUrl))
-            prepare()
-        }
-    }
-
-    companion object {
-        private const val TAG = "PlaybackVideoFragment"
+        playerView!!.player?.playWhenReady = true // 让播放器开始播放视频，无需修改代码。因此这里不需要添加新的代码块。
     }
 }
